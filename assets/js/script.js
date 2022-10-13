1ef7903986fc5ad88d1d030cf07e48e689f18687
ahsanaaxioned <ahsana@axioned.com>
2022-10-13 14:29:28 +0530

    js file added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
+// global variable declaration start here
+const galleryItem = document.querySelectorAll(".gallery-item");
+const modal = document.querySelector(".modal");
+const modalClose = document.querySelector(".modal-close");
+const overFlow =document.querySelector("html");
+// global variable declaration start here
+
+// Modal function start here
+galleryItem.forEach(function(image,idx){
+    image.addEventListener("click", function(){
+        let modalImg = document.querySelector(".modal-image");
+        let galleryImage = document.querySelectorAll(".gallery-img");
+        console.log(galleryImage[idx].src)
+        modalImg.src = galleryImage[idx].src;
+        modal.classList.add("appear");
+        overFlow.classList.add("remove-scroll");
+    });
+    modalClose.addEventListener("click",function(){
+        modal.classList.remove("appear");
+        overFlow.classList.remove("remove-scroll");
+    });
+});
+// Modal function end here
+
 
 
 
